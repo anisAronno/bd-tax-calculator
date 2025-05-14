542a351612db7ab49edbfe7e634cd59b8d9abe40
Anichur Rahaman <38912435+anisAronno@users.noreply.github.com>
2025-05-14 13:41:13 +0600

    Separate Footer and Added Note

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i5jdeen3imo"
+    "revision": "0.s6jq6fpf2v"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
